4e3a8e8b8480d2e4f7344b2fb81995b6b113ad9c
gabriel-primesw <gabriel@primesw.com.br>
2023-06-06 01:02:56 -0300

    v 0.0.6

--- src/ContatoDaoTest.java
+++ src/ContatoDaoTest.java
@@ -16,6 +16,7 @@ public class ContatoDaoTest {
         dataNascimento.set(1991, 4, 15);
         Contato novoContato = new Contato("Gabriel Felipe", "exerciciodao@gmail.com", "Rua 1, 1234, Parana, Brasil",
                 dataNascimento);
+
         dao.adiciona(novoContato);
 
         List<Contato> listaDepois = dao.lista();
@@ -30,17 +31,18 @@ public class ContatoDaoTest {
 
         ContatoDao dao = new ContatoDao();
 
-        List<Contato> listaContatosAntes = dao.lista();
-        Contato contatoExistente = listaContatosAntes.get(0);
+        List<Contato> listaAntes = dao.lista();
+        Contato contatoExistente = listaAntes.get(0);
 
         contatoExistente.setNome("Novo Nome");
         contatoExistente.setEmail("novoemail@example.com");
         contatoExistente.setEndereco("Novo Endereco");
         contatoExistente.setDataNascimento(Calendar.getInstance());
+
         dao.altera(contatoExistente);
 
-        List<Contato> listaContatosDepois = dao.lista();
-        Contato contatoAlterado = listaContatosDepois.get(0);
+        List<Contato> listaDepois = dao.lista();
+        Contato contatoAlterado = listaDepois.get(0);
 
         assertEquals("Novo Nome", contatoAlterado.getNome());
         assertEquals("novoemail@example.com", contatoAlterado.getEmail());
@@ -53,8 +55,6 @@ public class ContatoDaoTest {
         ContatoDao dao = new ContatoDao();
 
         List<Contato> listaContatos = dao.lista();
-        System.out.println(listaContatos);
-
         assertNotNull(listaContatos);
     }
 
@@ -63,15 +63,16 @@ public class ContatoDaoTest {
 
         ContatoDao dao = new ContatoDao();
 
-        List<Contato> listaContatosAntes = dao.lista();
+        List<Contato> listaAntes = dao.lista();
 
-        if (!listaContatosAntes.isEmpty()) {
-            Contato contatoRemover = listaContatosAntes.get(0);
+        if (!listaAntes.isEmpty()) {
+            Contato contatoRemover = listaAntes.get(0);
+
             dao.remove(contatoRemover);
 
-            List<Contato> listaContatosDepois = dao.lista();
-
-            assertEquals(listaContatosAntes.size() - 1, listaContatosDepois.size());
+            List<Contato> listaDepois = dao.lista();
+            assertEquals(listaAntes.size() - 1, listaDepois.size());
+            assertFalse(listaDepois.contains(contatoRemover));
         }
     }
 
